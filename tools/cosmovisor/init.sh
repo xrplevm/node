1e29e53a3e9ecda10566341572853a5a1483b807
AdriaCarrera <adria.cm95@gmail.com>
2024-08-29 14:02:24 +0200

    feat: add cosmovisor support for v3.0.0

--- tools/cosmovisor/init.sh
+++ tools/cosmovisor/init.sh
@@ -2,3 +2,9 @@
 
 mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
 ln -s /usr/local/bin/exrpd $DAEMON_HOME/cosmovisor/genesis/bin/exrpd
+mkdir -p $DAEMON_HOME/cosmovisor/upgrades/v2.0.0/bin
+cp /usr/local/bin/exrpd_v2.0.0 $DAEMON_HOME/cosmovisor/upgrades/v2.0.0/bin/exrpd
+mkdir -p $DAEMON_HOME/cosmovisor/upgrades/v3.0.0/bin
+cp /usr/local/bin/exrpd_v3.0.0 $DAEMON_HOME/cosmovisor/upgrades/v3.0.0/bin/exrpd
+
+ln -s $DAEMON_HOME/cosmovisor/upgrades/v2.0.0 $DAEMON_HOME/cosmovisor/current
