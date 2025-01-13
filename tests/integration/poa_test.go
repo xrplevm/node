3df4b72db6b59de648cb6f546785e7cc6427f10c
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-01-13 09:42:47 +0100

    feat(tests): add missing `MsgAddValidator` tests

--- tests/integration/poa_test.go
+++ tests/integration/poa_test.go
@@ -19,6 +19,8 @@ import (
 	poatypes "github.com/xrplevm/node/v5/x/poa/types"
 )
 
+// AddValidator tests
+
 func (s *TestSuite) TestAddValidator_UnexistingValidator() {
 	validator := s.Network().GetValidators()[0]
 	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
@@ -70,6 +72,72 @@ func (s *TestSuite) TestAddValidator_UnexistingValidator() {
 					ValidatorAddress: randomAcc.Address.String(),
 					Authority:        authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 					Pubkey:           msgPubKey,
+					Description: stakingtypes.Description{
+						Moniker: "test",
+					},
+				},
+			)
+
+			if tc.expectedError != nil && err != nil {
+				require.Error(s.T(), err)
+				require.ErrorIs(s.T(), err, tc.expectedError)
+			} else {
+				require.NoError(s.T(), err)
+			}
+
+			if tc.afterRun != nil {
+				tc.afterRun()
+			}
+		})
+	}
+}
+
+func (s *TestSuite) TestAddValidator_InvalidMsgAddValidator() {
+	// Generate a random account
+	randomAccs := simtypes.RandomAccounts(rand.New(rand.NewSource(time.Now().UnixNano())), 1)
+	randomAcc := randomAccs[0]
+	randomAccPubKey, _ := types1.NewAnyWithValue(randomAcc.ConsKey.PubKey())
+
+	validator := s.Network().GetValidators()[1]
+	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
+	require.NoError(s.T(), err)
+	valAccAddr := sdktypes.AccAddress(valAddr)
+
+	tt := []struct {
+		name          string
+		valAddress    string
+		valPubKey     *types1.Any 
+		expectedError error
+		beforeRun     func()
+		afterRun      func()
+	}{
+		{
+			name:       "add validator - already used pubkey",
+			valAddress: randomAcc.Address.String(),
+			valPubKey:  validator.ConsensusPubkey,
+			expectedError: stakingtypes.ErrValidatorPubKeyExists,
+		},
+		{
+			name:       "add validator - already used validator address",
+			valAddress: valAccAddr.String(),
+			valPubKey:  randomAccPubKey,
+			expectedError: poatypes.ErrAddressHasBankTokens,
+		},
+	}
+
+
+	for _, tc := range tt {
+		s.Run(tc.name, func() {
+			if tc.beforeRun != nil {
+				tc.beforeRun()
+			}
+
+			err := s.Network().PoaKeeper().ExecuteAddValidator(
+				s.Network().GetContext(),
+				&poatypes.MsgAddValidator{
+					ValidatorAddress: randomAcc.Address.String(),
+					Authority:        authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+					Pubkey:           validator.ConsensusPubkey,
 					Description: stakingtypes.Description{
 						Moniker: "test",
 					},
@@ -153,6 +221,212 @@ func (s *TestSuite) TestAddValidator_ExistingValidator_StatusBonded() {
 		})
 	}
 }
+
+func (s *TestSuite) TestAddValidator_ExistingValidator_Jailed() {
+	validator := s.Network().GetValidators()[1]
+	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
+	require.NoError(s.T(), err)
+	valAccAddr := sdktypes.AccAddress(valAddr)
+
+	tt := []struct {
+		name          string
+		valAddress    string
+		expectedError error
+		beforeRun     func()
+		afterRun      func()
+	}{
+		{
+			name:       "add existing validator - status jailed",
+			valAddress: valAccAddr.String(),
+			expectedError: poatypes.ErrAddressHasBondedTokens,
+			beforeRun: func() {
+				// Force jail validator
+				valSet := s.Network().GetValidatorSet()
+				// Exclude validator at index 1 from validator set
+				require.Equal(s.T(), sdktypes.ValAddress(valSet.Validators[1].Address).String(), valAddr.String())
+				vf := make([]cmtproto.BlockIDFlag, len(valSet.Validators))
+				for i := range valSet.Validators {
+					vf[i] = cmtproto.BlockIDFlagCommit
+				}
+				vf[1] = cmtproto.BlockIDFlagAbsent
+
+				require.NoError(s.T(), s.Network().NextNBlocksWithValidatorFlags(slashingtypes.DefaultSignedBlocksWindow+10, vf))
+
+				resVal, err := s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is jailed
+				require.Equal(s.T(), resVal.Validator.Jailed, true)
+
+				// Check if the validator is unbonding
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+			},
+			afterRun: func() {
+				// Check if the validator is still jailed
+				resVal, err := s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is jailed
+				require.Equal(s.T(), resVal.Validator.Jailed, true)
+
+				// Check if the validator is unbonding
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		s.Run(tc.name, func() {
+			if tc.beforeRun != nil {
+				tc.beforeRun()
+			}
+
+			err := s.Network().PoaKeeper().ExecuteAddValidator(
+				s.Network().GetContext(),
+				&poatypes.MsgAddValidator{
+					ValidatorAddress: valAccAddr.String(),
+					Authority:        authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+					Pubkey:           validator.ConsensusPubkey,
+					Description: stakingtypes.Description{
+						Moniker: "test",
+					},
+				},
+			)
+
+			if tc.expectedError != nil && err != nil {
+				require.Error(s.T(), err)
+				require.ErrorIs(s.T(), err, tc.expectedError)
+			} else {
+				require.NoError(s.T(), err)
+			}
+
+			if tc.afterRun != nil {
+				tc.afterRun()
+			}
+		})
+	}
+}
+
+func (s *TestSuite) TestAddValidator_ExistingValidator_Tombstoned() {
+	validator := s.Network().GetValidators()[1]
+	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
+	require.NoError(s.T(), err)
+	valAccAddr := sdktypes.AccAddress(valAddr)
+	valConsAddr := sdktypes.ConsAddress(valAddr)
+
+	tt := []struct {
+		name          string
+		valAddress    string
+		expectedError error
+		beforeRun     func()
+		afterRun      func()
+	}{
+		{
+			name:       "add existing validator - status tombstoned",
+			valAddress: valAccAddr.String(),
+			expectedError: poatypes.ErrAddressHasBondedTokens,
+			beforeRun: func() {
+				// Force validator to be tombstoned
+				require.NoError(s.T(), s.Network().NextBlockWithMisBehaviors(
+					[]abcitypes.Misbehavior{
+						{
+							Type: abcitypes.MisbehaviorType_DUPLICATE_VOTE,
+							Validator: abcitypes.Validator{
+								Address: valAddr.Bytes(),
+							},
+							Height:           s.Network().GetContext().BlockHeight(),
+							TotalVotingPower: s.Network().GetValidatorSet().TotalVotingPower(),
+						},
+					},
+				))
+
+				resVal, err := s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is jailed
+				require.Equal(s.T(), resVal.Validator.Jailed, true)
+
+				// Check if the validator is unbonding
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+
+				info, err := s.Network().GetSlashingClient().SigningInfo(
+					s.Network().GetContext(),
+					&slashingtypes.QuerySigningInfoRequest{
+						ConsAddress: valConsAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is tombstoned
+				require.Equal(s.T(), info.ValSigningInfo.Tombstoned, true)
+			},
+			afterRun: func() {
+				// Check if the validator is still jailed
+				resVal, err := s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is jailed
+				require.Equal(s.T(), resVal.Validator.Jailed, true)
+
+				// Check if the validator is unbonding
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		s.Run(tc.name, func() {
+			if tc.beforeRun != nil {
+				tc.beforeRun()
+			}
+
+			err := s.Network().PoaKeeper().ExecuteAddValidator(
+				s.Network().GetContext(),
+				&poatypes.MsgAddValidator{
+					ValidatorAddress: valAccAddr.String(),
+					Authority:        authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+					Pubkey:           validator.ConsensusPubkey,
+					Description: stakingtypes.Description{
+						Moniker: "test",
+					},
+				},
+			)
+
+			if tc.expectedError != nil && err != nil {
+				require.Error(s.T(), err)
+				require.ErrorIs(s.T(), err, tc.expectedError)
+			} else {
+				require.NoError(s.T(), err)
+			}
+
+			if tc.afterRun != nil {
+				tc.afterRun()
+			}
+		})
+	}
+}
+
+// RemoveValidator tests
 
 func (s *TestSuite) TestRemoveValidator_UnexistingValidator() {
 	// Generate a random account
