f031f1de68425660e859558e9199885196e67158
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-01-14 13:40:58 +0100

    feat(tests): remove validator integration tests

--- tests/integration/poa_test.go
+++ tests/integration/poa_test.go
@@ -1,18 +1,20 @@
 package integration
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
-	sdkmath "cosmossdk.io/math"
 	abcitypes "github.com/cometbft/cometbft/abci/types"
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
+	"github.com/xrplevm/node/v5/testutil/integration/exrp/utils"
 	poatypes "github.com/xrplevm/node/v5/x/poa/types"
 )
 
@@ -20,8 +22,10 @@ import (
 
 func (s *TestSuite) TestRemoveValidator_UnexistingValidator() {
 	// Generate a random account
-	randomAccs := simtypes.RandomAccounts(rand.New(rand.NewSource(time.Now().UnixNano())), 1)
+	randomAccs := simtypes.RandomAccounts(rand.New(rand.NewSource(time.Now().UnixNano())), 1) //nolint:gosec
 	randomAcc := randomAccs[0]
+
+	randomValAddr := sdktypes.ValAddress(randomAcc.Address.Bytes())
 
 	tt := []struct {
 		name          string
@@ -31,67 +35,30 @@ func (s *TestSuite) TestRemoveValidator_UnexistingValidator() {
 		afterRun      func()
 	}{
 		{
-			name:          "remove unexisting validator - random address - no balance",
-			valAddress:    randomAcc.Address.String(),
-			expectedError: poatypes.ErrAddressHasNoTokens,
-			beforeRun: func() {
-				balance, err := s.Network().GetBankClient().Balance(
-					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: randomAcc.Address.String(),
-						Denom:   s.Network().GetBondDenom(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check account has no balance
-				require.True(s.T(), balance.Balance.Amount.IsZero())
-			},
-		},
-		{
 			name:          "remove unexisting validator - random address - with balance",
 			valAddress:    randomAcc.Address.String(),
 			expectedError: poatypes.ErrAddressHasNoTokens,
 			beforeRun: func() {
-				coins := sdktypes.NewCoin(
-					s.Network().GetDenom(),
-					sdkmath.NewInt(100000000000),
-				)
-
-				err := s.factory.FundAccount(
-					s.keyring.GetKey(0),
-					randomAcc.Address,
-					sdktypes.NewCoins(
-						coins,
-					),
-				)
-				require.NoError(s.T(), err)
-				require.NoError(s.T(), s.Network().NextBlock())
-
-				balance, err := s.Network().GetBankClient().Balance(
+				_, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: randomAcc.Address.String(),
-						Denom:   s.Network().GetBondDenom(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: randomValAddr.String(),
 					},
 				)
-				require.NoError(s.T(), err)
 
-				// Check account has been funded
-				require.True(s.T(), balance.Balance.Amount.IsZero())
+				// Check if the validator does not exist
+				require.Contains(s.T(), err.Error(), fmt.Sprintf("validator %s not found", randomValAddr.String()))
 			},
 			afterRun: func() {
-				balance, err := s.Network().GetBankClient().Balance(
+				_, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: randomAcc.Address.String(),
-						Denom:   s.Network().GetBondDenom(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: randomValAddr.String(),
 					},
 				)
-				require.NoError(s.T(), err)
 
-				// Check account has been emptied
-				require.True(s.T(), balance.Balance.Amount.IsZero())
+				// Check if the validator is not found
+				require.Contains(s.T(), err.Error(), fmt.Sprintf("validator %s not found", randomValAddr.String()))
 			},
 		},
 	}
@@ -102,10 +69,17 @@ func (s *TestSuite) TestRemoveValidator_UnexistingValidator() {
 				tc.beforeRun()
 			}
 
-			err := s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
+			authority := sdktypes.AccAddress(address.Module("gov"))
+			msg := poatypes.NewMsgRemoveValidator(
+				authority.String(),
 				tc.valAddress,
 			)
+
+			proposal, err := utils.SubmitAndAwaitProposalResolution(s.factory, s.Network(), s.keyring.GetKeys(), "test", msg)
+			require.NoError(s.T(), err)
+
+			require.Equal(s.T(), govtypes.ProposalStatus_PROPOSAL_STATUS_FAILED, proposal.Status)
+			require.Equal(s.T(), tc.expectedError.Error(), proposal.FailedReason)
 
 			if tc.expectedError != nil && err != nil {
 				require.Error(s.T(), err)
@@ -126,7 +100,8 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_StatusBonded() {
 	validators := s.Network().GetValidators()
 	require.NotZero(s.T(), len(validators))
 
-	valAddr, err := sdktypes.ValAddressFromBech32(validators[0].OperatorAddress)
+	validator := validators[0]
+	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
 	require.NoError(s.T(), err)
 	valAccAddr := sdktypes.AccAddress(valAddr)
 
@@ -153,24 +128,12 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_StatusBonded() {
 				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Bonded)
 
 				// Check if the validator has delegator shares
-				require.False(s.T(), resVal.Validator.DelegatorShares.IsZero())
+				require.Equal(s.T(), sdktypes.DefaultPowerReduction.ToLegacyDec(), resVal.Validator.DelegatorShares)
 
 				// Check if the validator has tokens
 				require.NotZero(s.T(), resVal.Validator.Tokens)
 			},
 			afterRun: func() {
-				balance, err := s.Network().GetBankClient().Balance(
-					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: valAccAddr.String(),
-						Denom:   s.Network().GetDenom(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check account has been emptied
-				require.True(s.T(), balance.Balance.Amount.IsZero())
-
 				resVal, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
 					&stakingtypes.QueryValidatorRequest{
@@ -180,24 +143,56 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_StatusBonded() {
 				require.NoError(s.T(), err)
 
 				// Check if the validator has delegator shares
-				require.True(s.T(), resVal.Validator.DelegatorShares.IsZero())
+				require.True(s.T(),
+					resVal.Validator.DelegatorShares.IsZero(),
+					"delegator shares should be zero, got %s",
+					resVal.Validator.DelegatorShares,
+				)
 
 				// Check if the validator has no tokens
 				require.True(s.T(), resVal.Validator.Tokens.IsZero())
+
+				resVal, err = s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.NoError(s.T(), err)
+
+				// Check if the validator is unbonded
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+
+				require.NoError(s.T(), s.Network().NextBlockAfter(stakingtypes.DefaultUnbondingTime))
+
+				_, err = s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+				require.Contains(s.T(), err.Error(), fmt.Sprintf("validator %s not found", valAddr.String()))
 			},
 		},
 	}
 
+	//nolint:dupl
 	for _, tc := range tt {
 		s.Run(tc.name, func() {
 			if tc.beforeRun != nil {
 				tc.beforeRun()
 			}
 
-			err = s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
-				tc.valAddress,
+			authority := sdktypes.AccAddress(address.Module("gov"))
+			msg := poatypes.NewMsgRemoveValidator(
+				authority.String(),
+				valAccAddr.String(),
 			)
+
+			proposal, err := utils.SubmitAndAwaitProposalResolution(s.factory, s.Network(), s.keyring.GetKeys(), "test", msg)
+			require.NoError(s.T(), err)
+
+			require.Equal(s.T(), govtypes.ProposalStatus_PROPOSAL_STATUS_PASSED, proposal.Status)
 
 			if tc.expectedError != nil && err != nil {
 				require.Error(s.T(), err)
@@ -236,15 +231,17 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Jailed() {
 			beforeRun: func() {
 				// Force jail validator
 				valSet := s.Network().GetValidatorSet()
-				// Exclude validator at index 1 from validator set
-				require.Equal(s.T(), sdktypes.ValAddress(valSet.Validators[1].Address).String(), valAddr.String())
-				vf := make([]cmtproto.BlockIDFlag, len(valSet.Validators))
-				for i := range valSet.Validators {
-					vf[i] = cmtproto.BlockIDFlagCommit
-				}
-				vf[1] = cmtproto.BlockIDFlagAbsent
 
-				require.NoError(s.T(), s.Network().NextNBlocksWithValidatorFlags(slashingtypes.DefaultSignedBlocksWindow+10, vf))
+				require.NoError(
+					s.T(),
+					s.Network().NextNBlocksWithValidatorFlags(
+						slashingtypes.DefaultSignedBlocksWindow,
+						utils.NewValidatorFlags(
+							len(valSet.Validators),
+							utils.NewValidatorFlagOverride(1, cmtproto.BlockIDFlagAbsent),
+						),
+					),
+				)
 
 				resVal, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
@@ -261,19 +258,6 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Jailed() {
 				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
 			},
 			afterRun: func() {
-				// Check if the validator is jailed
-				balance, err := s.Network().GetBankClient().Balance(
-					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: valAccAddr.String(),
-						Denom:   s.Network().GetBondDenom(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check account has been emptied
-				require.True(s.T(), balance.Balance.Amount.IsZero())
-
 				resVal, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
 					&stakingtypes.QueryValidatorRequest{
@@ -287,20 +271,41 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Jailed() {
 
 				// Check if the validator has no tokens
 				require.True(s.T(), resVal.Validator.Tokens.IsZero())
+
+				// Check if the validator is unbonding
+				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
+
+				require.NoError(s.T(), s.Network().NextBlockAfter(stakingtypes.DefaultUnbondingTime))
+
+				_, err = s.Network().GetStakingClient().Validator(
+					s.Network().GetContext(),
+					&stakingtypes.QueryValidatorRequest{
+						ValidatorAddr: valAddr.String(),
+					},
+				)
+
+				require.Contains(s.T(), err.Error(), fmt.Sprintf("validator %s not found", valAddr.String()))
 			},
 		},
 	}
 
+	//nolint:dupl
 	for _, tc := range tt {
 		s.Run(tc.name, func() {
 			if tc.beforeRun != nil {
 				tc.beforeRun()
 			}
 
-			err = s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
-				tc.valAddress,
+			authority := sdktypes.AccAddress(address.Module("gov"))
+			msg := poatypes.NewMsgRemoveValidator(
+				authority.String(),
+				valAccAddr.String(),
 			)
+
+			proposal, err := utils.SubmitAndAwaitProposalResolution(s.factory, s.Network(), s.keyring.GetKeys(), "test", msg)
+			require.NoError(s.T(), err)
+
+			require.Equal(s.T(), govtypes.ProposalStatus_PROPOSAL_STATUS_PASSED, proposal.Status)
 
 			if tc.expectedError != nil && err != nil {
 				require.Error(s.T(), err)
@@ -317,15 +322,21 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Jailed() {
 }
 
 func (s *TestSuite) TestRemoveValidator_ExistingValidator_Tombstoned() {
-	// Validators
+	valIndex := 1
+
+	// CometBFT validators
+	valSet := s.Network().GetValidatorSet()
+	cmtValAddr := sdktypes.AccAddress(valSet.Validators[valIndex].Address.Bytes())
+	cmtValConsAddr := sdktypes.ConsAddress(valSet.Validators[valIndex].Address.Bytes())
+
+	// Cosmos validators
 	validators := s.Network().GetValidators()
 	require.NotZero(s.T(), len(validators))
 
-	validator := validators[1]
+	validator := validators[valIndex]
 	valAddr, err := sdktypes.ValAddressFromBech32(validator.OperatorAddress)
 	require.NoError(s.T(), err)
 	valAccAddr := sdktypes.AccAddress(valAddr)
-	valConsAddr := sdktypes.ConsAddress(valAddr)
 
 	tt := []struct {
 		name          string
@@ -344,7 +355,7 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Tombstoned() {
 						{
 							Type: abcitypes.MisbehaviorType_DUPLICATE_VOTE,
 							Validator: abcitypes.Validator{
-								Address: valAddr.Bytes(),
+								Address: cmtValAddr,
 							},
 							Height:           s.Network().GetContext().BlockHeight(),
 							TotalVotingPower: s.Network().GetValidatorSet().TotalVotingPower(),
@@ -369,7 +380,7 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Tombstoned() {
 				info, err := s.Network().GetSlashingClient().SigningInfo(
 					s.Network().GetContext(),
 					&slashingtypes.QuerySigningInfoRequest{
-						ConsAddress: valConsAddr.String(),
+						ConsAddress: cmtValConsAddr.String(),
 					},
 				)
 				require.NoError(s.T(), err)
@@ -378,18 +389,6 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Tombstoned() {
 				require.Equal(s.T(), info.ValSigningInfo.Tombstoned, true)
 			},
 			afterRun: func() {
-				balance, err := s.Network().GetBankClient().Balance(
-					s.Network().GetContext(),
-					&banktypes.QueryBalanceRequest{
-						Address: valAccAddr.String(),
-						Denom:   s.Network().GetBondDenom(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check account has been emptied
-				require.True(s.T(), balance.Balance.Amount.IsZero())
-
 				resVal, err := s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
 					&stakingtypes.QueryValidatorRequest{
@@ -403,184 +402,54 @@ func (s *TestSuite) TestRemoveValidator_ExistingValidator_Tombstoned() {
 
 				// Check if the validator has no tokens
 				require.True(s.T(), resVal.Validator.Tokens.IsZero())
-			},
-		},
-	}
-
-	for _, tc := range tt {
-		s.Run(tc.name, func() {
-			if tc.beforeRun != nil {
-				tc.beforeRun()
-			}
-
-			err = s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
-				tc.valAddress,
-			)
-
-			if tc.expectedError != nil && err != nil {
-				require.Error(s.T(), err)
-				require.ErrorIs(s.T(), err, tc.expectedError)
-			} else {
-				require.NoError(s.T(), err)
-			}
-
-			if tc.afterRun != nil {
-				tc.afterRun()
-			}
-		})
-	}
-}
-
-func (s *TestSuite) TestRemoveValidator_ExistingValidator_StatusUnbonded() {
-	// Validators
-	validators := s.Network().GetValidators()
-	require.NotZero(s.T(), len(validators))
-
-	valAddr, err := sdktypes.ValAddressFromBech32(validators[1].OperatorAddress)
-	require.NoError(s.T(), err)
-	valAccAddr := sdktypes.AccAddress(valAddr)
-
-	tt := []struct {
-		name          string
-		valAddress    string
-		expectedError error
-		beforeRun     func()
-		afterRun      func()
-	}{
-		{
-			name:          "remove existing validator - status unbonded",
-			valAddress:    valAccAddr.String(),
-			expectedError: poatypes.ErrAddressHasNoTokens,
-			beforeRun: func() {
-				// Force jail validator
-				valSet := s.Network().GetValidatorSet()
-				// Exclude validator at index 1 from validator set
-				require.Equal(s.T(), sdktypes.ValAddress(valSet.Validators[1].Address).String(), valAddr.String())
-				vf := make([]cmtproto.BlockIDFlag, len(valSet.Validators))
-				for i := range valSet.Validators {
-					vf[i] = cmtproto.BlockIDFlagCommit
-				}
-				vf[1] = cmtproto.BlockIDFlagAbsent
-
-				require.NoError(s.T(), s.Network().NextNBlocksWithValidatorFlags(slashingtypes.DefaultSignedBlocksWindow+10, vf))
-
-				resVal, err := s.Network().GetStakingClient().Validator(
-					s.Network().GetContext(),
-					&stakingtypes.QueryValidatorRequest{
-						ValidatorAddr: valAddr.String(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check if the validator is jailed
-				require.Equal(s.T(), resVal.Validator.Jailed, true)
 
 				// Check if the validator is unbonding
 				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
 
+				// Await unbonding time to pass
 				require.NoError(s.T(), s.Network().NextBlockAfter(stakingtypes.DefaultUnbondingTime))
-				require.NoError(s.T(), s.Network().NextBlock())
 
-				resVal, err = s.Network().GetStakingClient().Validator(
+				_, err = s.Network().GetStakingClient().Validator(
 					s.Network().GetContext(),
 					&stakingtypes.QueryValidatorRequest{
 						ValidatorAddr: valAddr.String(),
 					},
 				)
+
+				// Check if the validator is not found
+				require.Contains(s.T(), err.Error(), fmt.Sprintf("validator %s not found", valAddr.String()))
+
+				info, err := s.Network().GetSlashingClient().SigningInfo(
+					s.Network().GetContext(),
+					&slashingtypes.QuerySigningInfoRequest{
+						ConsAddress: cmtValConsAddr.String(),
+					},
+				)
 				require.NoError(s.T(), err)
 
-				// Check if the validator is unbonded
-				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonded)
+				// Check if the validator is tombstoned
+				require.True(s.T(), info.ValSigningInfo.Tombstoned)
 			},
 		},
 	}
 
+	//nolint:dupl
 	for _, tc := range tt {
 		s.Run(tc.name, func() {
 			if tc.beforeRun != nil {
 				tc.beforeRun()
 			}
 
-			err = s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
-				tc.valAddress,
+			authority := sdktypes.AccAddress(address.Module("gov"))
+			msg := poatypes.NewMsgRemoveValidator(
+				authority.String(),
+				valAccAddr.String(),
 			)
 
-			if tc.expectedError != nil && err != nil {
-				require.Error(s.T(), err)
-				require.ErrorIs(s.T(), err, tc.expectedError)
-			} else {
-				require.NoError(s.T(), err)
-			}
+			proposal, err := utils.SubmitAndAwaitProposalResolution(s.factory, s.Network(), s.keyring.GetKeys(), "test", msg)
+			require.NoError(s.T(), err)
 
-			if tc.afterRun != nil {
-				tc.afterRun()
-			}
-		})
-	}
-}
-
-func (s *TestSuite) TestRemoveValidator_ExistingValidator_StatusUnbonding() {
-	// Validators
-	validators := s.Network().GetValidators()
-	require.NotZero(s.T(), len(validators))
-
-	valAddr, err := sdktypes.ValAddressFromBech32(validators[1].OperatorAddress)
-	require.NoError(s.T(), err)
-	valAccAddr := sdktypes.AccAddress(valAddr)
-
-	tt := []struct {
-		name          string
-		valAddress    string
-		expectedError error
-		beforeRun     func()
-		afterRun      func()
-	}{
-		{
-			name:          "remove existing validator - status unbonded",
-			valAddress:    valAccAddr.String(),
-			expectedError: poatypes.ErrAddressHasNoTokens,
-			beforeRun: func() {
-				// Force jail validator
-				valSet := s.Network().GetValidatorSet()
-				// Exclude validator at index 1 from validator set
-				require.Equal(s.T(), sdktypes.ValAddress(valSet.Validators[1].Address).String(), valAddr.String())
-				vf := make([]cmtproto.BlockIDFlag, len(valSet.Validators))
-				for i := range valSet.Validators {
-					vf[i] = cmtproto.BlockIDFlagCommit
-				}
-				vf[1] = cmtproto.BlockIDFlagAbsent
-
-				require.NoError(s.T(), s.Network().NextNBlocksWithValidatorFlags(slashingtypes.DefaultSignedBlocksWindow+10, vf))
-
-				resVal, err := s.Network().GetStakingClient().Validator(
-					s.Network().GetContext(),
-					&stakingtypes.QueryValidatorRequest{
-						ValidatorAddr: valAddr.String(),
-					},
-				)
-				require.NoError(s.T(), err)
-
-				// Check if the validator is jailed
-				require.Equal(s.T(), resVal.Validator.Jailed, true)
-
-				// Check if the validator is unbonding
-				require.Equal(s.T(), resVal.Validator.Status, stakingtypes.Unbonding)
-			},
-		},
-	}
-
-	for _, tc := range tt {
-		s.Run(tc.name, func() {
-			if tc.beforeRun != nil {
-				tc.beforeRun()
-			}
-
-			err = s.Network().PoaKeeper().ExecuteRemoveValidator(
-				s.Network().GetContext(),
-				tc.valAddress,
-			)
+			require.Equal(s.T(), govtypes.ProposalStatus_PROPOSAL_STATUS_PASSED, proposal.Status)
 
 			if tc.expectedError != nil && err != nil {
 				require.Error(s.T(), err)
