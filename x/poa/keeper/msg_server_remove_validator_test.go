63f5ec8706387ae92d403c61615311209c0f757b
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-01-15 14:32:19 +0100

    fix(x/poa): msg server unit tests

--- x/poa/keeper/msg_server_remove_validator_test.go
+++ x/poa/keeper/msg_server_remove_validator_test.go
@@ -1,8 +1,10 @@
 package keeper
 
 import (
+	"errors"
 	"testing"
 
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/stretchr/testify/require"
 	"github.com/xrplevm/node/v5/x/poa/types"
 )
@@ -12,11 +14,46 @@ func TestMsgServer_RemoveValidator(t *testing.T) {
 
 	msgServer := NewMsgServerImpl(*poaKeeper)
 
-	msg := &types.MsgRemoveValidator{
-		Authority:        poaKeeper.GetAuthority(),
-		ValidatorAddress: "ethm1a0pd5cyew47pvgf7rd7axxy3humv9ev0nnkprp",
+	tt := []struct{
+		name string
+		authority string
+		validatorAddress string
+		expectedErr error
+	}{
+		{
+			name: "should fail - invalid authority address",
+			authority: "invalidauthority",
+			validatorAddress: "ethm1a0pd5cyew47pvgf7rd7axxy3humv9ev0nnkprp",
+			expectedErr: govtypes.ErrInvalidSigner,
+		},
+		{
+			name: "should fail - invalid validator address",
+			authority: poaKeeper.GetAuthority(),
+			validatorAddress: "invalidvalidatoraddress",
+			expectedErr: errors.New("decoding bech32 failed"),
+		},
+		{
+			name:  "should pass",
+			authority: poaKeeper.GetAuthority(),
+			validatorAddress: "ethm1a0pd5cyew47pvgf7rd7axxy3humv9ev0nnkprp",
+		},
 	}
 
-	_, err := msgServer.RemoveValidator(ctx, msg)
-	require.NoError(t, err)
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			msg := &types.MsgRemoveValidator{
+				Authority:        tc.authority,
+				ValidatorAddress: tc.validatorAddress,
+			}
+
+			_, err := msgServer.RemoveValidator(ctx, msg)
+			if tc.expectedErr != nil {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expectedErr.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+
 }
