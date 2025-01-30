dd1c4a6fba2bfc04bf4612f9df9c60ceee125f53
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-01-30 16:14:49 +0100

    feat(tests): upgrade staking module tests

---
+++ app/upgrades/v7/integration/staking_test.go
@@ -0,0 +1,69 @@
+package integration
+
+import (
+	"time"
+
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+func (s *UpgradeTestSuite) TestUpgrade_Staking_Params() {
+	prevParams, err := s.network.GetStakingClient().Params(
+		s.network.GetContext(),
+		&stakingtypes.QueryParamsRequest{},
+	)
+	s.Require().NoError(err)
+
+	s.RunUpgrade(upgradeName)
+
+	postParams, err := s.network.GetStakingClient().Params(
+		s.network.GetContext(),
+		&stakingtypes.QueryParamsRequest{},
+	)
+	s.Require().NoError(err)
+
+	// Check that not modified params are the same
+	s.Require().Equal(prevParams.Params.BondDenom, postParams.Params.BondDenom)
+	s.Require().Equal(prevParams.Params.MaxValidators, postParams.Params.MaxValidators)
+	s.Require().Equal(prevParams.Params.MinCommissionRate, postParams.Params.MinCommissionRate)
+	s.Require().Equal(prevParams.Params.MaxEntries, postParams.Params.MaxEntries)
+	s.Require().Equal(prevParams.Params.HistoricalEntries, postParams.Params.HistoricalEntries)
+
+	// Check that unbonding time was modified
+	s.Require().Equal(postParams.Params.UnbondingTime, 100*time.Second)
+}
+
+func (s *UpgradeTestSuite) TestUpgrade_Staking_Validators() {
+	res, err := s.network.GetStakingClient().Validators(
+		s.network.GetContext(),
+		&stakingtypes.QueryValidatorsRequest{},
+	)
+	s.Require().NoError(err)
+
+	s.RunUpgrade(upgradeName)
+
+	postRes, err := s.network.GetStakingClient().Validators(
+		s.network.GetContext(),
+		&stakingtypes.QueryValidatorsRequest{},
+	)
+	s.Require().NoError(err)
+
+	// Check that not modified validators are the same
+	s.Require().Equal(res.Validators, postRes.Validators)
+}
+
+func (s *UpgradeTestSuite) TestUpgrade_Staking_Delegations() {
+	prevDelegations, err := s.network.StakingKeeper().GetAllDelegations(
+		s.network.GetContext(),
+	)
+	s.Require().NoError(err)
+
+	s.RunUpgrade(upgradeName)
+
+	postDelegations, err := s.network.StakingKeeper().GetAllDelegations(
+		s.network.GetContext(),
+	)
+	s.Require().NoError(err)
+
+	// Check that not modified delegations are the same
+	s.Require().Equal(prevDelegations, postDelegations)
+}
