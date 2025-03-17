a2e7e9866d0cc158704d6c2c206f5bf3c4965aaf
AdriaCarrera <adria.cm95@gmail.com>
2025-03-17 11:58:27 +0100

    fix: IF-FINDING-001 Invariant checks through crisis module may go unnoticed

--- x/poa/module.go
+++ x/poa/module.go
@@ -127,9 +127,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 }
 
 // RegisterInvariants registers the invariants of the module. If an invariant deviates from its predicted value, the InvariantRegistry triggers appropriate logic (most often the chain will be halted)
-func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
-	keeper.RegisterInvariants(ir, am.keeper)
-}
+func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // InitGenesis performs the module's genesis initialization. It returns no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) {
