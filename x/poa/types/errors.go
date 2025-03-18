112af0b5489a0e81ddbdeb1447bb27e8f5266ee8
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-03-18 13:48:48 +0100

    fix(x/poa): poa errors code

--- x/poa/types/errors.go
+++ x/poa/types/errors.go
@@ -8,11 +8,11 @@ import (
 
 // x/poa module sentinel errors
 var (
-	ErrAddressHasBankTokens      = sdkerrors.Register(ModuleName, 2, "address already has bank tokens")
-	ErrAddressHasBondedTokens    = sdkerrors.Register(ModuleName, 4, "address already has bonded tokens")
-	ErrAddressHasUnbondingTokens = sdkerrors.Register(ModuleName, 5, "address already has unbonding tokens")
-	ErrAddressHasDelegatedTokens = sdkerrors.Register(ModuleName, 7, "address already has delegated tokens")
-	ErrInvalidValidatorStatus    = sdkerrors.Register(ModuleName, 8, "invalid validator status")
-	ErrAddressIsNotAValidator    = sdkerrors.Register(ModuleName, 9, "address is not a validator")
-	ErrMaxValidatorsReached      = sdkerrors.Register(ModuleName, 10, "maximum number of validators reached")
+	ErrAddressHasBankTokens      = sdkerrors.Register(ModuleName, 1, "address already has bank tokens")
+	ErrAddressHasBondedTokens    = sdkerrors.Register(ModuleName, 2, "address already has bonded tokens")
+	ErrAddressHasUnbondingTokens = sdkerrors.Register(ModuleName, 3, "address already has unbonding tokens")
+	ErrAddressHasDelegatedTokens = sdkerrors.Register(ModuleName, 4, "address already has delegated tokens")
+	ErrInvalidValidatorStatus    = sdkerrors.Register(ModuleName, 5, "invalid validator status")
+	ErrAddressIsNotAValidator    = sdkerrors.Register(ModuleName, 6, "address is not a validator")
+	ErrMaxValidatorsReached      = sdkerrors.Register(ModuleName, 7, "maximum number of validators reached")
 )
