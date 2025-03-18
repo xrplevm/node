1198ebf3caa1900b8a9da4fb0d417293d7c3d7cc
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-03-18 13:34:46 +0100

    fix(x/poa): remove unused errors

--- x/poa/types/errors.go
+++ x/poa/types/errors.go
@@ -9,10 +9,8 @@ import (
 // x/poa module sentinel errors
 var (
 	ErrAddressHasBankTokens      = sdkerrors.Register(ModuleName, 2, "address already has bank tokens")
-	ErrAddressHasNoTokens        = sdkerrors.Register(ModuleName, 3, "address has not tokens")
 	ErrAddressHasBondedTokens    = sdkerrors.Register(ModuleName, 4, "address already has bonded tokens")
 	ErrAddressHasUnbondingTokens = sdkerrors.Register(ModuleName, 5, "address already has unbonding tokens")
-	ErrAddressHasUnbondedTokens  = sdkerrors.Register(ModuleName, 6, "address already has unbonded tokens")
 	ErrAddressHasDelegatedTokens = sdkerrors.Register(ModuleName, 7, "address already has delegated tokens")
 	ErrInvalidValidatorStatus    = sdkerrors.Register(ModuleName, 8, "invalid validator status")
 	ErrAddressIsNotAValidator    = sdkerrors.Register(ModuleName, 9, "address is not a validator")
