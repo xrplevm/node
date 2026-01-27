d8b6bddcdd997267e6d126f4c05762f74f756105
AdriaCarrera <adria.cm95@gmail.com>
2026-01-27 13:10:09 +0100

    chore: remove unused ERC20 interface contracts

--- contracts/precompiles/erc20/IERC20Mintable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "./IERC20.sol";
-
-interface IERC20Mintable is IERC20 {
-   
-    function mint(address account, uint256 amount) external returns (bool);
-}
