b1ea512b14dfbc290b36cd73e051feb5dc422599
AdriaCarrera <adria.cm95@gmail.com>
2026-02-20 12:53:24 +0100

    feat: add change of xrp owner and upgrade version

--- cmd/exrpd/cmd/in-place-testnet.go
+++ cmd/exrpd/cmd/in-place-testnet.go
@@ -381,9 +381,9 @@ func updateApplicationState(app *gaia.App, args valArgs) error {
 
 	// UPGRADE
 	if args.upgradeVersion != "" {
-		fmt.Println("Scheduling upgrade to", app.LastBlockHeight(), "at height", app.LastBlockHeight()+5, "with name", args.upgradeVersion)
+		upgradeHeight := app.LastBlockHeight() + 5
 		upgradePlan := upgradetypes.Plan{
-			Height: app.LastBlockHeight() + 5,
+			Height: upgradeHeight,
 			Name:   args.upgradeVersion,
 			Info:   "Testnet upgrade to " + args.upgradeVersion,
 		}
@@ -391,7 +391,7 @@ func updateApplicationState(app *gaia.App, args valArgs) error {
 			panic(
 				fmt.Errorf(
 					"failed to schedule upgrade %s during BeginBlock at height %d: %w",
-					upgradePlan.Name, app.LastBlockHeight()+5, err,
+					upgradePlan.Name, upgradeHeight, err,
 				),
 			)
 		}
